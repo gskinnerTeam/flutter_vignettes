e5c1b48e4c26110f2143cae3050a00ef759afe03
Shawn <shawn@gskinner.com>
2024-02-27 10:54:19 -0700

    Fix casting issues

--- vignettes/_shared/lib/ui/widget_mask.dart
+++ vignettes/_shared/lib/ui/widget_mask.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-
-
 /// Acts exactly like a `Stack` however the first child acts like an alpha mask when rendering the rest of the children
 class RenderWidgetMask extends RenderStack {
   RenderWidgetMask(
       {List<RenderBox>? children,
-        required AlignmentGeometry alignment,
-        required TextDirection textDirection,
-        required StackFit fit,
-        required Clip clip})
+      required AlignmentGeometry alignment,
+      required TextDirection textDirection,
+      required StackFit fit,
+      required Clip clip})
       : super(
-            children: children,
-            alignment: alignment,
-            textDirection: textDirection,
-            fit: fit,
-            clipBehavior: clip);
+          children: children,
+          alignment: alignment,
+          textDirection: textDirection,
+          fit: fit,
+          clipBehavior: clip,
+        );
 
   @override
   void paintStack(context, offset) {
@@ -25,22 +24,24 @@ class RenderWidgetMask extends RenderStack {
 
     final paintContent = (PaintingContext context, Offset offset) {
       // Paint all but the first child
-      RenderBox? child = (firstChild?.parentData as StackParentData).nextSibling;
+      RenderBox? child = (firstChild?.parentData as StackParentData?)?.nextSibling;
       while (child != null) {
-        final childParentData = child.parentData as StackParentData;
-        context.paintChild(lastChild as RenderObject, offset + childParentData.offset);
-        child = childParentData.nextSibling;
+        final childParentData = child.parentData as StackParentData?;
+        final lastChildRenderObject = lastChild as RenderObject?;
+        if (childParentData != null && lastChildRenderObject != null) {
+          context.paintChild(lastChildRenderObject, offset + childParentData.offset);
+        }
+        child = childParentData?.nextSibling;
       }
     };
 
     final paintMask = (PaintingContext context, Offset offset) {
-      context.paintChild(firstChild as RenderObject,
-          offset + (firstChild?.parentData as StackParentData).offset);
+      context.paintChild(firstChild as RenderObject, offset + (firstChild?.parentData as StackParentData).offset);
     };
 
     final paintEverything = (PaintingContext context, Offset offset) {
       paintContent(context, offset);
-      context.canvas.saveLayer(offset & size, Paint()..blendMode=BlendMode.dstIn);
+      context.canvas.saveLayer(offset & size, Paint()..blendMode = BlendMode.dstIn);
       paintMask(context, offset);
       context.canvas.restore();
     };
