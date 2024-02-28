15424248bf6d102f9acf49de5077256257753e17
Shawn <shawn@gskinner.com>
2024-02-28 13:16:44 -0700

    Update particle_swipe to nnbd, update android build files, remove maxOverscrollFactor from /shared/constrained_scroll_physics

--- vignettes/particle_swipe/lib/demo.dart
+++ vignettes/particle_swipe/lib/demo.dart
@@ -1,15 +1,13 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
+import 'components/sprite_sheet.dart';
 import 'demo_data.dart';
 import 'list_model.dart';
 import 'main.dart';
 import 'particle_field.dart';
 import 'particle_field_painter.dart';
 import 'removed_swipe_item.dart';
-import 'components/sprite_sheet.dart';
 import 'swipe_item.dart';
 
 class ParticleSwipeDemo extends StatefulWidget {
@@ -26,10 +24,10 @@ class ParticleSwipeDemo extends StatefulWidget {
 class ParticleSwipeDemoState extends State<ParticleSwipeDemo> with SingleTickerProviderStateMixin {
   //static const double headerHeight = 80;
   GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
-  ListModel _model;
-  SpriteSheet _spriteSheet;
-  ParticleField _particleField;
-  Ticker _ticker;
+  late ListModel _model;
+  late SpriteSheet _spriteSheet;
+  ParticleField _particleField = ParticleField();
+  late Ticker _ticker = createTicker(_particleField.tick);
 
   @override
   void initState() {
@@ -48,9 +46,7 @@ class ParticleSwipeDemoState extends State<ParticleSwipeDemo> with SingleTickerP
       removedItemBuilder: (dynamic removedItem, BuildContext context, Animation<double> animation) =>
           RemovedSwipeItem(animation: animation),
     );
-
-    _particleField = ParticleField();
-    _ticker = createTicker(_particleField.tick)..start();
+    _ticker.start();
     super.initState();
   }
 
@@ -62,7 +58,6 @@ class ParticleSwipeDemoState extends State<ParticleSwipeDemo> with SingleTickerP
 
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
     // Draw the header and List UI with a ParticleFieldPainter layered over top:
     return Stack(children: <Widget>[
       _buildList(),
@@ -80,33 +75,38 @@ class ParticleSwipeDemoState extends State<ParticleSwipeDemo> with SingleTickerP
       physics: ClampingScrollPhysics(),
       itemBuilder: (BuildContext context, int index, _) {
         var item = _model[index];
-        return SwipeItem(data: item, isEven: index.isEven, onSwipe: (key, {action}) => _performSwipeAction(item, key, action));
+        return SwipeItem(
+            data: item,
+            isEven: index.isEven,
+            onSwipe: (key, {required action}) => _performSwipeAction(item, key, action));
       },
     );
   }
 
   void _performSwipeAction(Email data, GlobalKey key, SwipeAction action) {
     // Get item's render box, and use it to calculate the position for the particle effect:
-    final RenderBox box = key.currentContext.findRenderObject();
-    Offset position = box.localToGlobal(Offset.zero, ancestor: context.findRenderObject());
-    double x = position.dx;
-    double y = position.dy;
-    double w = box.size.width;
+    final box = key.currentContext?.findRenderObject() as RenderBox?;
+    if (box != null) {
+      Offset position = box.localToGlobal(Offset.zero, ancestor: context.findRenderObject());
+      double x = position.dx;
+      double y = position.dy;
+      double w = box.size.width;
 
-    if (action == SwipeAction.remove) {
-      // Delay the start of the effect a little bit, so the item is mostly closed before it begins.
-      Future.delayed(Duration(milliseconds: 100)).then((_) => _particleField.lineExplosion(x, y, w));
+      if (action == SwipeAction.remove) {
+        // Delay the start of the effect a little bit, so the item is mostly closed before it begins.
+        Future.delayed(Duration(milliseconds: 100)).then((_) => _particleField.lineExplosion(x, y, w));
 
-      // Remove the item (using the ItemModel to sync everything), and redraw the UI (to update count):
-      setState(() {
-        _model.removeAt(_model.indexOf(data), duration: Duration(milliseconds: 200));
-        widget.data.remove(data);
-      });
-    }
-    if (action == SwipeAction.favorite) {
-      data.toggleFavorite();
-      if (data.isFavorite) {
-        _particleField.pointExplosion(x + 60, y + 46, 100);
+        // Remove the item (using the ItemModel to sync everything), and redraw the UI (to update count):
+        setState(() {
+          _model.removeAt(_model.indexOf(data), duration: Duration(milliseconds: 200));
+          widget.data.remove(data);
+        });
+      }
+      if (action == SwipeAction.favorite) {
+        data.toggleFavorite();
+        if (data.isFavorite) {
+          _particleField.pointExplosion(x + 60, y + 46, 100);
+        }
       }
     }
   }
