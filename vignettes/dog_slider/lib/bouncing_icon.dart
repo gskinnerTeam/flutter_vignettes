0ab707f17b1fc429e93f8b291922750f815f2040
Shawn <shawn@gskinner.com>
2024-02-28 11:46:29 -0700

    Update dog_slider for nnbd, update android build files

--- vignettes/dog_slider/lib/bouncing_icon.dart
+++ vignettes/dog_slider/lib/bouncing_icon.dart
@@ -5,7 +5,7 @@ class BouncingWidget extends StatefulWidget {
   final Widget child;
   final double maxBounce;
 
-  const BouncingWidget({Key key, this.isVisible, this.child, this.maxBounce = 20}) : super(key: key);
+  const BouncingWidget({Key? key, this.isVisible = false, required this.child, this.maxBounce = 20}) : super(key: key);
 
   @override
   _BouncingWidgetState createState() => _BouncingWidgetState();
@@ -23,18 +23,20 @@ class _BouncingWidgetState extends State<BouncingWidget> {
   }
 
   void _bounceIcon(bool up) async {
-    _bounceCurve = up? Curves.easeOut : Curves.easeIn;
-    setState(() => _offsetY = up? 0 : 20);
+    _bounceCurve = up ? Curves.easeOut : Curves.easeIn;
+    setState(() => _offsetY = up ? 0 : 20);
     await Future.delayed(_bounceDuration);
-    if(mounted){ _bounceIcon(!up); }
+    if (mounted) {
+      _bounceIcon(!up);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return AnimatedOpacity(
-      opacity: widget.isVisible? 1 : 0,
+      opacity: widget.isVisible ? 1 : 0,
       duration: Duration(milliseconds: 350),
-      child: TweenAnimationBuilder(
+      child: TweenAnimationBuilder<double>(
         tween: Tween<double>(begin: 0, end: _offsetY),
         curve: _bounceCurve,
         duration: _bounceDuration,
