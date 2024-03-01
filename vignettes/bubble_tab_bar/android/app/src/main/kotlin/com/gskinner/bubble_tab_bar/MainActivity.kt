caef8043a9c3499ce41e830aaeb9605c20201386
Soroush Beigi <sbeygi712@gmail.com>
2024-03-01 11:10:27 +0330

    dart sdk and android build files

--- vignettes/bubble_tab_bar/android/app/src/main/kotlin/com/gskinner/bubble_tab_bar/MainActivity.kt
+++ vignettes/bubble_tab_bar/android/app/src/main/kotlin/com/gskinner/bubble_tab_bar/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.gskinner.bubble_tab_bar
 
-import android.os.Bundle
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
-}
+class MainActivity: FlutterActivity()
